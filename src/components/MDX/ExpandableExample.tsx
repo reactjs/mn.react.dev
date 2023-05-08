9522095a57260de1bf372572c306fe1dec82d91a
Munkhjin Badamkhatan <munkhjin0223@gmail.com>
2023-05-08 16:54:07 +0800

    fix text

--- src/components/MDX/ExpandableExample.tsx
+++ src/components/MDX/ExpandableExample.tsx
@@ -101,7 +101,7 @@ function ExpandableExample({children, excerpt, type}: ExpandableExampleProps) {
           <span className="mr-1">
             <IconChevron displayDirection={isExpanded ? 'up' : 'down'} />
           </span>
-          {isExpanded ? 'Хурааангуй' : 'Дэлгэрэнгүй'}
+          {isExpanded ? 'Хураангуй' : 'Дэлгэрэнгүй'}
         </Button>
       </summary>
       <div
